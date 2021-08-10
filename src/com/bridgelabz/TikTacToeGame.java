6430389b2136e51665e9f1945dfcc7a5dee15bfa
Megha <meghajagadale99@gmail.com>
2021-08-10 10:28:23 +0530

    Added: tiktactoe game assign aaray value 10

--- src/com/bridgelabz/TikTacToeGame.java
+++ src/com/bridgelabz/TikTacToeGame.java
@@ -1,9 +1,19 @@
 package com.bridgelabz;
 
 public class TikTacToeGame {
-        public static void main(String[] args) {
-            System.out.println("Welcome to Tik Tac Toe Game");
-        }
+    public static void main(String[] args) {
+        System.out.println("Welcome to Tik Tac Toe Game");
+        createBoard();
     }
+
+    public static char[] createBoard() {
+        char[] board = new char[10];    // initializing size of board
+        for (int index = 1; index < board.length; index++) {
+
+            board[index] = ' ';   // assigning empty value
+        }
+        return board;
+    }
+}
 
 
