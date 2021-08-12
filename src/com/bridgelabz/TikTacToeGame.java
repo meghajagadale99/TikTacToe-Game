3f83d18b031c24ed358980ed7d46af45ef1ddfda
Megha <meghajagadale99@gmail.com>
2021-08-12 12:32:30 +0530

    Added: player choose letter X or O

--- src/com/bridgelabz/TikTacToeGame.java
+++ src/com/bridgelabz/TikTacToeGame.java
@@ -1,10 +1,9 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class TikTacToeGame {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Tik Tac Toe Game");
-        createBoard();
-    }
+    static char PLAYER, COMPUTER;
 
     public static char[] createBoard() {
         char[] board = new char[10];    // initializing size of board
@@ -13,6 +12,31 @@ public class TikTacToeGame {
             board[index] = ' ';   // assigning empty value
         }
         return board;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Tik Tac Toe Game");
+        char[] board = createBoard();
+        System.out.println("Select the letter \n press 1. Choose X \n press 2.Choose O \n Enter your choice:");
+        Scanner sc = new Scanner(System.in);
+        int choice = sc.nextInt();
+        switch (choice) {
+            case 1:
+                PLAYER = 'X';
+                COMPUTER = 'O';
+                break;
+            case 2:
+                PLAYER = 'O';
+                COMPUTER = 'X';
+                break;
+
+            default:
+                System.out.println("Invalid Choice ! Retry Again!");
+                return;
+
+        }
+
     }
 }
 
