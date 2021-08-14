7e52df546951a87319ebcb21e623ffaea101d2c7
Megha <meghajagadale99@gmail.com>
2021-08-14 20:27:13 +0530

    Added: winner or tie or change the turn

--- src/com/bridgelabz/TikTacToeGame.java
+++ src/com/bridgelabz/TikTacToeGame.java
@@ -6,7 +6,7 @@ public class TikTacToeGame {
     static char PLAYER, COMPUTER;
     private static char[] board = new char[10];
     static int User, computerNo;
-    static int turn =1, flag =0;
+    static int turn = 1, flag = 0;
 
     public static void main(String[] args) {
         System.out.println("Welcome to Tic Tac Toe Game");
@@ -14,11 +14,23 @@ public class TikTacToeGame {
         choosingXorO();
         showBoard();
         tossCoin();
-        while (flag==0){
-            if ((turn+1)%2==0){
+        outerloop:
+        while (flag == 0) {
+            if ((turn + 1) % 2 == 0) {
                 currentBoard();
                 userCall();
                 userMove();
+                currentBoard();
+                flag = checkWin();
+                if (flag == 1) {
+                    System.out.println("Excellent! You are the winner");
+                    break outerloop;
+                }
+                flag = checkTie();
+                if (flag == 1) {
+                    System.out.println("Nice Play! It's Tie");
+                    break outerloop;
+                }
                 turn++;
             }
         }
@@ -36,35 +48,38 @@ public class TikTacToeGame {
         System.out.println("Choose 1 for 'X' or Choose 2 for 'O' as your mark");
         int option = Utility.getUserInteger();
         switch (option) {
-            case 1: PLAYER = 'X';
-                       COMPUTER = 'O';
-                             break;
-            case 2: PLAYER = 'O';
-                      COMPUTER = 'X';
-                              break;
+            case 1:
+                PLAYER = 'X';
+                COMPUTER = 'O';
+                break;
+            case 2:
+                PLAYER = 'O';
+                COMPUTER = 'X';
+                break;
             default:
                 System.out.println("Your input is invalid");
                 choosingXorO();
         }
-        System.out.println("\n PLAYER: '"+PLAYER+"' and COMPUTER: '"+COMPUTER+"'");
+        System.out.println("\n PLAYER: '" + PLAYER + "' and COMPUTER: '" + COMPUTER + "'");
     }
 
     private static void showBoard() {
-        System.out.println("\n  "+board[1]+" | "+board[2]+" | "+board[3]+" ");
+        System.out.println("\n  " + board[1] + " | " + board[2] + " | " + board[3] + " ");
         System.out.println(" ----------- ");
-        System.out.println("  "+board[4]+" | "+board[5]+" | "+board[6]+" ");
+        System.out.println("  " + board[4] + " | " + board[5] + " | " + board[6] + " ");
         System.out.println(" ----------- ");
-        System.out.println("  "+board[7]+" | "+board[8]+" | "+board[9]+" \n");
+        System.out.println("  " + board[7] + " | " + board[8] + " | " + board[9] + " \n");
     }
 
     private static void currentBoard() {
-        for (int i=1;i<10;i++) {
-            if (board[i] !='X'&&board[i] !='O') {
-                board[i]=(char) (i+'0');
+        for (int i = 1; i < 10; i++) {
+            if (board[i] != 'X' && board[i] != 'O') {
+                board[i] = (char) (i + '0');
             }
         }
         showBoard();
     }
+
     private static void userCall() {
         System.out.println("\nEnter a number from board to make the mark:\n");
         User = Utility.getUserInteger();
@@ -74,22 +89,25 @@ public class TikTacToeGame {
             userCall();
         }
     }
+
     private static void userMove() {
-        if(board[User]=='X' || board[User]=='O') {
+        if (board[User] == 'X' || board[User] == 'O') {
             currentBoard();
             System.out.println("Number which is selected is not free");
             userCall();
             userMove();
         } else {
-            board[User]=PLAYER;
+            board[User] = PLAYER;
         }
     }
+
     private static void tossCoin() {
         System.out.println("\nMaking a toss to check who plays first\nChoose 1 for Head or 2 for Tail");
-        int option = Utility.getUserInteger();;
-        if ( option==1 || option==2 ) {
-            int flip = Utility.getRandomInt(2)+1;
-            if (flip==1) {
+        int option = Utility.getUserInteger();
+        ;
+        if (option == 1 || option == 2) {
+            int flip = Utility.getRandomInt(2) + 1;
+            if (flip == 1) {
                 System.out.println("\nBy tossing Coin it shows HEAD\n");
             } else {
                 System.out.println("\nBy tossing Coin it shows TAIL\n");
@@ -107,8 +125,57 @@ public class TikTacToeGame {
     }
 
     public static void computerFirstTurn() {
-        computerNo = Utility.getRandomInt(9)+1;
-        board[computerNo]=PLAYER;
-        System.out.println("Computer chooses '"+computerNo+"' now user turn");
+        computerNo = Utility.getRandomInt(9) + 1;
+        board[computerNo] = PLAYER;
+        System.out.println("Computer chooses '" + computerNo + "' now user turn");
+    }
+
+    public static int checkWin() {
+        for (int i = 1; i < 9; i++) {
+            int win[] = winArray(i);
+            if (board[win[0]] == board[win[1]] && board[win[1]] == board[win[2]]) {
+                flag = 1;
+            }
+        }
+        return flag;
+    }
+
+    private static int[] winArray(int number) {
+        if (number == 1) {
+            int arrayWin[] = {1, 2, 3};
+            return arrayWin;
+        } else if (number == 2) {
+            int arrayWin[] = {4, 5, 6};
+            return arrayWin;
+        } else if (number == 3) {
+            int arrayWin[] = {7, 8, 9};
+            return arrayWin;
+        } else if (number == 4) {
+            int arrayWin[] = {1, 4, 7};
+            return arrayWin;
+        } else if (number == 5) {
+            int arrayWin[] = {2, 5, 8};
+            return arrayWin;
+        } else if (number == 6) {
+            int arrayWin[] = {3, 6, 9};
+            return arrayWin;
+        } else if (number == 7) {
+            int arrayWin[] = {1, 5, 9};
+            return arrayWin;
+        } else {
+            int arrayWin[] = {3, 5, 7};
+            return arrayWin;
+        }
+    }
+
+    public static int checkTie() {
+        for (int i = 1; i < 10; i++) {
+            if (board[i] == 'X' || board[i] == 'O') {
+                if (i == 9) {
+                    flag = 1;
+                }
+            }
+        }
+        return flag;
     }
 }
