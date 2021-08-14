0a0eb862cf7e4fb19b11d090acc27f88672e50e9
Megha <meghajagadale99@gmail.com>
2021-08-14 17:30:53 +0530

    Added: player choose letter X or O

--- src/com/bridgelabz/TikTacToeGame.java
+++ src/com/bridgelabz/TikTacToeGame.java
@@ -6,54 +6,34 @@ public class TikTacToeGame {
     static char PLAYER, COMPUTER;
     static char[] board = new char[10];
 
-    public static char[] createBoard() {
-        // initializing size of board
-        for (int index = 1; index < board.length; index++) {
-
-            board[index] = ' ';   // assigning empty value
-        }
-        return board;
-    }
-
-    public static void showBoard() {
-        System.out.println("Tic Tac Toe Board");
-        System.out.println("| " + board[1] + " | " + board[2] + " | " + board[3] + " |");
-        System.out.println("|-----------|");
-        System.out.println("| " + board[4] + " | " + board[5] + " | " + board[6] + " |");
-        System.out.println("|-----------|");
-        System.out.println("| " + board[7] + " | " + board[8] + " | " + board[9] + " |");
-    }
-
-
     public static void main(String[] args) {
-        System.out.println("Welcome to Tik Tac Toe Game");
-        char[] board = createBoard();
-        System.out.println("Select the letter \n press 1. Choose X \n press 2.Choose O \n Enter your choice:");
-        Scanner sc = new Scanner(System.in);
-        int choice = sc.nextInt();
-        switch (choice) {
+        System.out.println("Welcome to Tic Tac Toe Game");
+        createBoard();
+        choosingXorO();
+    }
+
+    private static void createBoard() {
+        for (int i = 1; i < 10; i++) {
+            board[i] = ' ';
+        }
+    }
+
+    private static void choosingXorO() {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Choose 1 for 'X' or Choose 2 for 'O' as your mark");
+        int option = s.nextInt();
+        switch (option) {
             case 1:
                 PLAYER = 'X';
                 COMPUTER = 'O';
                 break;
-
             case 2:
                 PLAYER = 'O';
                 COMPUTER = 'X';
                 break;
-
             default:
-                System.out.println("Invalid Choice ! Retry Again!");
-                return;
+                System.out.println("Your input is invalid");
+                choosingXorO();
         }
-        showBoard();
-        sc.close();
     }
 }
-
-
-
-
-
-
-
