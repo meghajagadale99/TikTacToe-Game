46d13808dcf9b383f15278d7e56e29bbad06558c
Megha <meghajagadale99@gmail.com>
2021-08-14 17:43:06 +0530

    Added: display current board

--- src/com/bridgelabz/TikTacToeGame.java
+++ src/com/bridgelabz/TikTacToeGame.java
@@ -10,9 +10,11 @@ public class TikTacToeGame {
         System.out.println("Welcome to Tic Tac Toe Game");
         createBoard();
         choosingXorO();
+        currentBoard();
     }
 
     private static void createBoard() {
+        board = new char[10];
         for (int i = 1; i < 10; i++) {
             board[i] = ' ';
         }
@@ -23,17 +25,32 @@ public class TikTacToeGame {
         System.out.println("Choose 1 for 'X' or Choose 2 for 'O' as your mark");
         int option = s.nextInt();
         switch (option) {
-            case 1:
-                PLAYER = 'X';
+            case 1: PLAYER = 'X';
                 COMPUTER = 'O';
                 break;
-            case 2:
-                PLAYER = 'O';
+            case 2: PLAYER = 'O';
                 COMPUTER = 'X';
                 break;
             default:
                 System.out.println("Your input is invalid");
                 choosingXorO();
         }
+    }
+
+    private static void showBoard() {
+        System.out.println("\n  "+board[1]+" | "+board[2]+" | "+board[3]+" ");
+        System.out.println(" ----------- ");
+        System.out.println("  "+board[4]+" | "+board[5]+" | "+board[6]+" ");
+        System.out.println(" ----------- ");
+        System.out.println("  "+board[7]+" | "+board[8]+" | "+board[9]+" \n");
+    }
+
+    private static void currentBoard() {
+        for (int i=1;i<10;i++) {
+            if (board[i] !='X'&&board[i] !='O') {
+                board[i]=(char) (i+'0');
+            }
+        }
+        showBoard();
     }
 }
