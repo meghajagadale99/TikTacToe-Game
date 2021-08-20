2c04d9aedca3bb6cf5cf3ab4a63c2896e9429c69
Megha <meghajagadale99@gmail.com>
2021-08-20 23:25:41 +0530

    Added: player choose letter X or Y

--- src/com/bridgelabz/TikTacToeGame.java
+++ src/com/bridgelabz/TikTacToeGame.java
@@ -1,5 +1,5 @@
 package com.bridgelabz;
-
+import java.util.Scanner;
 public class TikTacToeGame {
     static char[] board = new char[10];
 
@@ -9,10 +9,26 @@ public class TikTacToeGame {
             board[i] = ' ';
         }
     }
+    public static void chooseLetter(){
+        System.out.println("Choose letter 'X' or 'O' (in Caps) : ");
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        if (str.contains("X")){
+            char userSymbol='X';
+            char compSymbol='O';
+            System.out.println("User Symbol : "+userSymbol+" ; Comp Symbol : "+compSymbol);
+        }
+        else{
+            char userSymbol='O';
+            char compSymbol='X';
+            System.out.println("User Symbol : "+userSymbol+" ; Comp Symbol : "+compSymbol);
+        }
+    }
 
     public static void main(String[] args) {
         System.out.println("Welcome to Tic Tac Toe Game");
         generateBoard();
+        chooseLetter();
     }
 
 }
