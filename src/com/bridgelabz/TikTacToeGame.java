9defff4466f119351a75a4199bd6151cc1993ea7
Megha <meghajagadale99@gmail.com>
2021-08-20 11:12:11 +0530

    Added:corners are not available then take the Centre

--- src/com/bridgelabz/TikTacToeGame.java
+++ src/com/bridgelabz/TikTacToeGame.java
@@ -51,6 +51,12 @@ public class TikTacToeGame {
                     flag=0;
                     return;
                 }
+                flag=computerCenterSide();
+                if (flag==1) {
+                    turn++;
+                    flag=0;
+                    return;
+                }
             }
             }
         }
@@ -247,6 +253,25 @@ public class TikTacToeGame {
                 System.out.println("Cmputer choice is '"+corner[i]+"'");
                 flag=1;
                 break;
+            }
+        }
+        return flag;
+    }
+
+    private static int computerCenterSide() {
+        if (board[5] != 'X' && board[5] != 'O') {
+            board[5]=COMPUTER;
+            System.out.println("Computer choice is '5'");
+            flag=1;
+        } else {
+            int side[] = {2,6,8,4};
+            for(int j=0;j<4;j++) {
+                if(board[side[j]] != 'X' && board[side[j]] != 'O') {
+                    board[side[j]]=COMPUTER;
+                    System.out.println("My choice is '"+side[j]+"'");
+                    flag=1;
+                    break;
+                }
             }
         }
         return flag;
