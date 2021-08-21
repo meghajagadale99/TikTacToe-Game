b12e48c80906b55cef60fe051405cd0bd86caf80
Megha <meghajagadale99@gmail.com>
2021-08-21 08:21:08 +0530

    Added:check free spaces

--- src/com/bridgelabz/TikTacToeGame.java
+++ src/com/bridgelabz/TikTacToeGame.java
@@ -3,6 +3,7 @@ package com.bridgelabz;
 import java.util.Scanner;
 
 public class TikTacToeGame {
+
     static char userSymbol, compSymbol;
     private static char[] board;
     static int userInput;
@@ -15,38 +16,37 @@ public class TikTacToeGame {
         }
     }
 
-    public static void chooseLetter() {
+    public static void chooseLetter(){
         System.out.println("Choose letter 'X' or 'O' (in Caps) : ");
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
-        if (str.contains("X")) {
-            char userSymbol = 'X';
-            char compSymbol = 'O';
-            System.out.println("User Symbol : " + userSymbol + " ; Comp Symbol : " + compSymbol);
-        } else {
-            char userSymbol = 'O';
-            char compSymbol = 'X';
-            System.out.println("User Symbol : " + userSymbol + " ; Comp Symbol : " + compSymbol);
+        if (str.contains("X")){
+            char userSymbol='X';
+            char compSymbol='O';
+            System.out.println("User Symbol : "+userSymbol+" ; Comp Symbol : "+compSymbol);
+        }
+        else{
+            char userSymbol='O';
+            char compSymbol='X';
+            System.out.println("User Symbol : "+userSymbol+" ; Comp Symbol : "+compSymbol);
         }
     }
-
-    static void printBoard() {
-        System.out.println("\n  " + board[1] + " | " + board[2] + " | " + board[3] + " ");
+    static void printBoard()
+    {
+        System.out.println("\n  "+board[1]+" | "+board[2]+" | "+board[3]+" ");
         System.out.println(" ----------- ");
-        System.out.println("  " + board[4] + " | " + board[5] + " | " + board[6] + " ");
+        System.out.println("  "+board[4]+" | "+board[5]+" | "+board[6]+" ");
         System.out.println(" ----------- ");
-        System.out.println("  " + board[7] + " | " + board[8] + " | " + board[9] + " \n");
+        System.out.println("  "+board[7]+" | "+board[8]+" | "+board[9]+" \n");
     }
-
     private static void currentBoard() {
-        for (int i = 1; i < 10; i++) {
-            if (board[i] != 'X' && board[i] != 'O') {
-                board[i] = (char) (i + '0');
+        for (int i=1;i<10;i++) {
+            if (board[i] != 'X' && board[i] !='O') {
+                board[i]=(char) (i+'0');
             }
         }
         printBoard();
     }
-
     private static void userMove() {
         Scanner s = new Scanner(System.in);
         System.out.println("Enter your move : ");
@@ -57,6 +57,16 @@ public class TikTacToeGame {
             userMove();
         }
     }
+    private static void userMoveCheck() {
+        if(board[userInput]=='X' || board[userInput]=='O') {
+            currentBoard();
+            System.out.println("Number which is selected is not free");
+            userMove();
+            userMoveCheck();
+        } else {
+            board[userInput]=userSymbol;
+        }
+    }
 
     public static void main(String[] args) {
         System.out.println("Welcome to Tic Tac Toe Game");
@@ -65,6 +75,7 @@ public class TikTacToeGame {
         printBoard();
         currentBoard();
         userMove();
+        userMoveCheck();
     }
 
 
